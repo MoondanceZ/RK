a57273747bb5d6ca09126b8efea01db0abc94797
MoondanceZ <QYK_zz@hotmail.com>
2018-01-19 15:47:08 +0800

    Fix: DbContext

--- RK.Framework/Database/Impl/DatabaseFactory.cs
+++ RK.Framework/Database/Impl/DatabaseFactory.cs
@@ -8,30 +8,28 @@ namespace RK.Framework.Database.Impl
 {
     public class DatabaseFactory : IDatabaseFactory, IDesignTimeDbContextFactory<RkDbContext>
     {
-        private readonly RkDbContext _dbContext;
-
-        public DatabaseFactory()
-        {
-            _dbContext = CreateDbContext(null);
-        }
+        private static readonly RkDbContext _dbContext = new RkDbContext(new DbContextOptionsBuilder<RkDbContext>().UseMySql(ConfigHelper.GetConnectionString("ConnStr")).Options);
+        
         public RkDbContext DataContext
         {
             get
             {
+                var hashCode = _dbContext.GetHashCode();
                 return _dbContext;
             }
         }
 
         public RkDbContext CreateDbContext(string[] args)
         {
-            var builder = new DbContextOptionsBuilder<RkDbContext>();
-            var connStr = ConfigHelper.GetConnectionString("ConnStr");
-            builder.UseMySql(connStr);
+            //var builder = new DbContextOptionsBuilder<RkDbContext>();
+            //var connStr = ConfigHelper.GetConnectionString("ConnStr");
+            //builder.UseMySql(connStr);
 
-            //Ensure database creation
-            var dataContext = new RkDbContext(builder.Options);
-            //dataContext.Database.EnsureCreated();  //删除这句  因为执行 Update-Database 报错
-            return dataContext;
+            ////Ensure database creation
+            //var dataContext = new RkDbContext(builder.Options);
+            ////dataContext.Database.EnsureCreated();  //删除这句  因为执行 Update-Database 报错
+            //return dataContext;
+            return _dbContext;
         }
     }
 }
