cbee12777d8cd23d3993fe0b5a454a3c05e1264e
MoondanceZ <QYK_zz@hotmail.com>
2018-01-25 17:09:42 +0800

    FIx 计算每日金额

--- RK.Model/Dto/Reponse/DateAccountResponse.cs
+++ RK.Model/Dto/Reponse/DateAccountResponse.cs
@@ -7,7 +7,7 @@ namespace RK.Model.Dto.Reponse
     public class DateAccountResponse
     {
         public string Date { get; set; }
-        public decimal DateAmount { get; set; }
+        public string DateAmount { get; set; }
         public List<AccountResponse> AccountRecords { get; set; }
     }
 }
