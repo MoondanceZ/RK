15f919293577252e204d65e8d07f52eccd72bd73
MoondanceZ <QYK_zZ@hotmail.com>
2018-07-19 21:53:34 +0800

    update core_v2.1

---
+++ RK.Api/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RK.Infrastructure;
+
+namespace RK.Api.Controllers
+{    
+    [AllowAnonymous]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Json(ReturnStatus.Success("test success"));
+        }
+    }
+}
